ab5b11d662a7c2400f2058626a8611b10635cbc5
Jared <jaredcanonigo@gmail.com>
2022-06-26 18:21:22 -0700

    Test commit, changed comment name.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const App = () => {
     return (
         <div className="ui container comments">
          <CommentDetail author="Jared" time ="5:01" comment = "good morning! " />
-         <CommentDetail author="TJ"  time ="5:02" comment = "nice one!"/>
+         <CommentDetail author="Luis"  time ="5:02" comment = "nice one!"/>
          <CommentDetail author="Francesca" time ="5:03" comment = "helllloo!"/>
         </div>
 
